35655acf88a126a31adc43a61eadcab5e5be28a6
AngeloEkisBokis <thedjshadowgames@gmail.com>
2026-07-20 18:35:03 -0300

    bleh

--- js/plugins/Redimere_Framework.js
+++ js/plugins/Redimere_Framework.js
@@ -1,1 +1,236 @@
+/*:
+ * @target MZ
+ * @plugindesc Redimere Framework v0.1.0
+ * @author Limones Studio
+ * @url https://github.com/AngeloEkisBokis/Redimere
+ *
+ * @help
+ * ============================================================================
+ * Redimere Framework
+ * ============================================================================
+ *
+ * Framework base utilizado por todos os plugins do projeto Redimere.
+ *
+ * Este plugin deve permanecer SEMPRE acima dos demais plugins
+ * Redimere no Plugin Manager.
+ *
+ */
+
+(() => {
+
+"use strict";
+
+//==========================================================================
+// Namespace
+//==========================================================================
+
+window.Redimere = window.Redimere || {};
+
+const RDM = window.Redimere;
+
+//==========================================================================
+// Version
+//==========================================================================
+
+RDM.version = "0.1.0";
+
+//==========================================================================
+// Plugin Registry
+//==========================================================================
+
+RDM.plugins = {};
+
+//==========================================================================
+// Logger
+//==========================================================================
+
+RDM.log = function(message){
+
+    console.log("[Redimere] " + message);
+
+};
+
+//==========================================================================
+// Error
+//==========================================================================
+
+RDM.error = function(message){
+
+    console.error("[Redimere] " + message);
+
+};
+
+//==========================================================================
+// Register Plugin
+//==========================================================================
+
+RDM.registerPlugin = function(name, version){
+
+    this.plugins[name] = {
+
+        version: version
+
+    };
+
+    this.log(name + " v" + version + " carregado.");
+
+};
+
+//==========================================================================
+// Utils
+//==========================================================================
+
+RDM.utils = {};
+
+RDM.utils.clamp = function(value, min, max){
+
+    return Math.max(min, Math.min(max, value));
+
+};
+
+RDM.utils.lerp = function(start, end, amount){
+
+    return start + (end - start) * amount;
+
+};
+
+RDM.utils.centerX = function(sprite){
+
+    sprite.x = Graphics.width / 2;
+
+};
+
+RDM.utils.centerY = function(sprite){
+
+    sprite.y = Graphics.height / 2;
+
+};
+
+RDM.utils.center = function(sprite){
+
+    this.centerX(sprite);
+
+    this.centerY(sprite);
+
+};
+
+//==========================================================================
+// Startup
+//==========================================================================
+
+//==========================================================================
+// Core
+//==========================================================================
+
+RDM.Core = {};
+
+RDM.Core.name = "Redimere Framework";
+
+RDM.Core.author = "Limones Studio";
+
+RDM.Core.version = RDM.version;
+
+//==========================================================================
+// Audio
+//==========================================================================
+
+RDM.Audio = {};
+
+RDM.Audio.playSE = function(name, volume = 90, pitch = 100, pan = 0){
+
+    AudioManager.playSe({
+
+        name: name,
+
+        volume: volume,
+
+        pitch: pitch,
+
+        pan: pan
+
+    });
+
+};
+
+RDM.Audio.playBGM = function(name, volume = 90, pitch = 100, pan = 0){
+
+    AudioManager.playBgm({
+
+        name: name,
+
+        volume: volume,
+
+        pitch: pitch,
+
+        pan: pan
+
+    });
+
+};
+
+//==========================================================================
+// Scene
+//==========================================================================
+
+RDM.Scene = {};
+
+RDM.Scene.goto = function(scene){
+
+    SceneManager.goto(scene);
+
+};
+
+//==========================================================================
+// Screen
+//==========================================================================
+
+RDM.Screen = {};
+
+RDM.Screen.fadeOut = function(duration = 30){
+
+    if(SceneManager._scene){
+
+        SceneManager._scene.startFadeOut(duration,false);
+
+    }
+
+};
+
+RDM.Screen.fadeIn = function(duration = 30){
+
+    if(SceneManager._scene){
+
+        SceneManager._scene.startFadeIn(duration,false);
+
+    }
+
+};
+
+//==========================================================================
+// Banner
+//==========================================================================
+
+console.log(
+"%c========================================",
+"color:#66ffcc;"
+);
+
+console.log(
+"%c REDIMERE FRAMEWORK " + RDM.version,
+"color:#66ffcc;font-weight:bold;"
+);
+
+console.log(
+"%c Limones Studio",
+"color:white;"
+);
+
+console.log(
+"%c========================================",
+"color:#66ffcc;"
+);
+
+RDM.log("Framework inicializado.");
+
+})();
 
--- js/plugins/Redimere_Startup.js
+++ js/plugins/Redimere_Startup.js
@@ -1,1 +1,108 @@
+/*:
+ * @target MZ
+ * @plugindesc Redimere Startup System v0.1.0
+ * @author Limones Studio
+ *
+ * @help
+ * ============================================================================
+ * Redimere Startup System
+ * ============================================================================
+ *
+ * Exibe a logo do estúdio antes da tela de título.
+ *
+ * ============================================================================
+ *
+ * Estrutura esperada:
+ *
+ * img/system/LimonesStudio.png
+ * audio/se/Gear.wav
+ *
+ * ============================================================================
+ *
+ * @param Logo Image
+ * @text Logo
+ * @type file
+ * @dir img/system/
+ * @default LimonesStudio
+ *
+ * @param Sound Effect
+ * @text Som
+ * @type file
+ * @dir audio/se/
+ * @default Gear
+ *
+ * @param Fade In
+ * @type number
+ * @default 60
+ *
+ * @param Hold Time
+ * @type number
+ * @default 180
+ *
+ * @param Fade Out
+ * @type number
+ * @default 60
+ *
+ * @param Volume
+ * @type number
+ * @default 90
+ *
+ * @param Pitch
+ * @type number
+ * @default 100
+ *
+ */
 
+(() => {
+
+"use strict";
+
+const pluginName = "Redimere_Startup";
+
+const params = PluginManager.parameters(pluginName);
+
+const Config = {
+
+    logo: String(params["Logo Image"] || "LimonesStudio"),
+
+    sound: String(params["Sound Effect"] || "Gear"),
+
+    fadeIn: Number(params["Fade In"] || 60),
+
+    hold: Number(params["Hold Time"] || 180),
+
+    fadeOut: Number(params["Fade Out"] || 60),
+
+    volume: Number(params["Volume"] || 90),
+
+    pitch: Number(params["Pitch"] || 100)
+
+};
+
+window.Redimere = window.Redimere || {};
+
+Redimere.Startup = {};
+
+Redimere.Startup.Version = "0.1.0";
+
+Redimere.Startup.Config = Config;
+
+Redimere.Startup.log = function(text){
+
+    console.log("[Redimere Startup] " + text);
+
+};
+
+const StartupState = {
+
+    FadeIn : 0,
+
+    Wait : 1,
+
+    FadeOut : 2,
+
+    Finish : 3
+
+};
+
+})();
